a336aeb4163d00b1a4fcd3f0d978b5a8523cdf42
Lahiru Dinushan Rajapaksha <lahiruzone@gmail.com>
2019-07-19 04:26:18 +0000

    user create

---
+++ app/src/main/java/com/kodiaxlanka/famuspeople/CreateUser.java
@@ -0,0 +1,15 @@
+package com.kodiaxlanka.famuspeople;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_user);
+    }
+}
